ef0ed05bb3879535dd726a304bf9dfeff2b49941
surubhisharma11 <surubhisharma1998@gmail.com>
2019-11-06 03:24:39 -0800

    commiyt

--- Hibernate_Proj1/src/main/java/com/lti/Hibernate_Maven/Hibernate_Proj1/App.java
+++ Hibernate_Proj1/src/main/java/com/lti/Hibernate_Maven/Hibernate_Proj1/App.java
@@ -1,5 +1,10 @@
 package com.lti.Hibernate_Maven.Hibernate_Proj1;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,22 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
+    	Configuration configuration=new Configuration().configure();
+    	StandardServiceRegistryBuilder builder=new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+    	
+    	
+    	SessionFactory factory=configuration.buildSessionFactory(builder.build());
+    	Session session=factory.openSession();
+    	Employeee emp=new Employeee();
+    	emp.setEmpName("aaaaaa");
+    	emp.setBranch("agra");
+    	session.beginTransaction();
+    	session.save(emp);
+    	session.getTransaction().commit();
+    	
+    	
+    	
         System.out.println( "Hello World!" );
     }
 }
